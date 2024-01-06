35aaca80ff246212ad149009a5fe6cfd6223db48
vektemok <lunnoa15@gmail.com>
2024-01-06 07:57:27 +0300

    add styles

--- lib/src/features/education/survey/presentation/screens/one_answer_screen.dart
+++ lib/src/features/education/survey/presentation/screens/one_answer_screen.dart
@@ -11,13 +11,56 @@ class _OneAnswerScreenState extends State<OneAnswerScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: SafeArea(
         child: Column(
           children: <Widget>[
-            TextField(
-              maxLines: 20,
-              decoration: InputDecoration(border: OutlineInputBorder()),
-            )
+            const Flexible(
+                child: Padding(
+              padding: EdgeInsets.only(left: 16),
+              child: Text(
+                'Напишите эссе на тему синдром самозванца:',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 24,
+                    letterSpacing: -0.48,
+                    fontFamily: 'GoogleSans'),
+              ),
+            )),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                maxLines: 15,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                        borderSide: const BorderSide(
+                            color: Color.fromRGBO(147, 147, 147, 1),
+                            width: 2))),
+              ),
+            ),
+            const Spacer(),
+            Padding(
+              padding: const EdgeInsets.only(top: 16, bottom: 12),
+              child: SizedBox(
+                height: 42,
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Дальше',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromRGBO(96, 101, 214, 1)),
+                ),
+              ),
+            ),
           ],
         ),
       ),
--- lib/src/features/education/videos/presentation/screens/education_video_screen.dart
+++ lib/src/features/education/videos/presentation/screens/education_video_screen.dart
@@ -9,7 +9,6 @@ class EducationVideoScreen extends StatefulWidget {
 }
 
 class _EducationVideoScreenState extends State<EducationVideoScreen> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,7 +30,29 @@ class _EducationVideoScreenState extends State<EducationVideoScreen> {
               'Спикер: Михаил Ледер',
               style: speakerTextStyle,
             ),
-
+            const Spacer(),
+            Padding(
+              padding: const EdgeInsets.only(top: 16, bottom: 12),
+              child: SizedBox(
+                height: 42,
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => EducationVideoScreen())),
+                  child: Text(
+                    'Начать',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromRGBO(96, 101, 214, 1)),
+                ),
+              ),
+            ),
           ],
         ),
       )),
